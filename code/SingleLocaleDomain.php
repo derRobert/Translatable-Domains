14bc3630ecbbe560fde4bc1bae39ea49a1bfd132
Robert Schwandner <rs@rob24.de>
2013-10-03 17:37:32 +0200

    SS 3.1 Fixes

--- code/SingleLocaleDomain.php
+++ code/SingleLocaleDomain.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * The default behavior of Silverstripe is to show any page in any locale on the 
- * same top level domain.  This class is designed to look up the locale of a 
- * requested page, and change the url to the appropriate domain (set in 
+ * The default behavior of Silverstripe is to show any page in any locale on the
+ * same top level domain.  This class is designed to look up the locale of a
+ * requested page, and change the url to the appropriate domain (set in
  * TranslatableDomains class)
  *
  */
 
-class SingleLocaleDomain extends DataObjectDecorator{
-	
+class SingleLocaleDomain extends DataExtension{
+
 	/**
 	 * ignored_url_segments
 	 * Array of URLSegments to exclude from domain-locale rules
@@ -17,23 +17,23 @@ class SingleLocaleDomain extends DataObjectDecorator{
 	 * will redirect to default locale's domain and viewing content would not be possible.
 	 *
 	 */
-	 
-	static $ignored_url_segments = array(
+
+	private static $ignored_url_segments = array(
 		'Security',
 		'dev',
 		'admin'
 	);
-	
+
 	public function addIgnoredURLSegment($urlSegment=null){
 		if(!in_array($urlSegment, self::$ignored_url_segments)) $ignored_url_segments[] = $urlSegment;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * ContentController
 	 * lets us extend init() through this method on SiteTree.
-	 * we will override page init to change domains if the page locale is 
+	 * we will override page init to change domains if the page locale is
 	 * different than the detected locale of the domain.
 	 *
 	 * 3 Things to be aware of (while developing..  these are all accounted for here):
@@ -42,50 +42,51 @@ class SingleLocaleDomain extends DataObjectDecorator{
 	 *		� i18n locale that is in the header.
 	 *
 	 */
-	 
-	
+
+
 	public function contentcontrollerInit(){
-	 
+
 		if($this->owner->hasExtension('Translatable')){
 			//find the correct locale
 			$curLoc = TranslatableDomains::getLocaleFromHost();
 			// compare page locale vs domain's locale
 			// low occurance of these not matching, but important
-			
+
 			if(Translatable::get_current_locale() != $curLoc && !in_array($this->owner->URLSegment, self::$ignored_url_segments)){
 				// check to see if the page has a translation for the url, if so, translate.
 				// helpful for homepages where / == /home but we want the german translation..
-				
+
 				if($this->owner->hasTranslation($curLoc)){
-					//if page exists and translation exists, redirect & show translation	
+					//if page exists and translation exists, redirect & show translation
 					$correctPage = $this->owner->getTranslation($curLoc);
-					Director::redirect($correctPage->Link());
+                    //Director::redirect($correctPage->Link());
+					Controller::curr()->redirect($correctPage->Link());
 				} else {
 					//otherwise, find requested page by url, determine locale, and put us in the right domain.
 					$newUrl = TranslatableDomains::convertLocaleToTLD($withEndSlash=false).$this->owner->Link();
-					Director::redirect($newUrl);
+					Controller::curr()->redirect($newUrl);
 				}
 			} else i18n::set_locale($this->owner->Locale);
 		}
 	}
-	
-	
+
+
 	/**
          * alternateAbsoluteLink
          * changes absoluteLink() to include the correct domain.
          * commonly used in language switchers and when viewing draft / published pages in the CMS
-	 * 
+	 *
 	 */
-	
+
 	function alternateAbsoluteLink($action=null) {
 		$segment = ($action) ? "/".$action : '';
 		return TranslatableDomains::convertLocaleToTLD($withEndSlash=false).Director::baseURL().$this->owner->URLSegment.'/'.$segment;
 	}
-	
+
 	/**
 	 * other helpful utility methods
 	 */
-	
+
 	/**
 	 * PageByCurrentLocale
 	 * gets a page in the default locale and finds its translation in the current locale
@@ -93,14 +94,14 @@ class SingleLocaleDomain extends DataObjectDecorator{
 	 * @param string $pageURL url of a page in the default locale
 	 * @return DataObject Translated record of the requested page in the current locale, null if none exists.
 	 */
-	
+
 	function PageByCurrentLocale($pageURL) {
 		if($pg = Translatable::get_one_by_locale('Page', Translatable::default_locale(), "URLSegment = '{$pageURL}'")) return $pg->getTranslation(Translatable::get_current_locale());
-		
+
 		return null;
 	}
-	
-	
+
+
 	/**
 	 * PageByDefaultLocale
 	 * gets a page in the default locale
@@ -108,12 +109,12 @@ class SingleLocaleDomain extends DataObjectDecorator{
 	 * @param string $pageURL url of a page in the default locale
 	 * @return DataObject requested page in the current locale, null if none exists.
 	 */
-	 
+
 	function PageByDefaultLocale($pageURL){
 		$defLoc = Translatable::default_locale();
 		if($pg = Translatable::get_one_by_locale('Page', $defLoc, "URLSegment = '{$pageURL}'")) return $pg;
-		
+
 		return null;
 	}
-	
+
 }
--- code/TranslatableDomains.php
+++ code/TranslatableDomains.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * TranslatableDomains helps determine top level domains, and handle 
+ * TranslatableDomains helps determine top level domains, and handle
  * registration of top level domains on silverstripe multi-lingual websites.
  *
  * untested with subsites module,
@@ -11,49 +11,49 @@
  */
 
 class TranslatableDomains{
-	
+
 	/**
 	 * domain_locale_map
 	 * Arrays of top-level-domains that map to locales.
 	 */
-	
-	static $domain_locale_map = array(
+
+	private static $domain_locale_map = array(
 		//tld(.com, .net, .co.uk, etc) => locale(en_US, etc)
 	);
-	
-	
+
+
 	/**
 	 * addDomainHandler
 	 * Method to add domains and locales to the {@link domain_locale_map}.
 	 * errors will be thrown if developer tries to add the same tld more than once.
-	 * 
+	 *
 	 * @param string $domain one top level domain (.com, .co.uk, .fr)
 	 * @param string $locale one locale that should be mapped to the $domain param.
 	 */
-	
+
 	function addDomainHandler($domain, $locale){
 		if(isset(self::$domain_locale_map[$domain])) user_error("Overwriting domain", E_USER_WARNING);
 		self::$domain_locale_map[$domain] = $locale;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * getLocaleFromHost
-	 * finds the tld from the server (www.mysite.com = com), then returns 
-	 * the locale from the tld in the array $domain_locale_map 
+	 * finds the tld from the server (www.mysite.com = com), then returns
+	 * the locale from the tld in the array $domain_locale_map
 	 *
 	 * uses {@getLocaleFromURL} to look up the tld from the server.
-	 *	
+	 *
 	 * @return locale associated with the current top level domain.
 	 *
 	 */
-	
-	public function getLocaleFromHost(){
+
+	public static function getLocaleFromHost(){
 		$locale = self::getLocaleFromURL($_SERVER["HTTP_HOST"]);
 		return $locale;
 	}
-	
+
 	/**
 	 * getLocaleFromURL
 	 * finds the locale of the url passed into the function, and returns the associated locale
@@ -62,37 +62,38 @@ class TranslatableDomains{
 	 * @return locale associated with tld: (.de == de_DE)
 	 *
 	 */
-	
-	public function getLocaleFromURL($host){
+
+	public static function getLocaleFromURL($host){
+        $domain_map = Config::inst()->get('TranslatableDomains', 'domain_locale_map');
 		$locale = i18n::default_locale();
 		//be able to return default locale if nothing is found.
-		
-		if($tld = self::getTLD($host)) $locale = self::$domain_locale_map[$tld];
+		if($tld = self::getTLD($host)) $locale = $domain_map[$tld];
 		return $locale;
 	}
-	
+
 	/**
 	 * getTLD
-	 * finds the tld from the host parameter (www.mysite.com = com) 
+	 * finds the tld from the host parameter (www.mysite.com = com)
 	 *
 	 * @param string $host URL used to retrieve tld from.
 	 * @return tld associated with com: en_US (or whatever developer specifies)
 	 *
 	 */
-	
-	public function getTLD($host){
-		foreach (self::$domain_locale_map as $tld => $locale) {
+
+	public static function getTLD($host){
+		foreach (Config::inst()->get('TranslatableDomains', 'domain_locale_map') as $tld => $locale) {
 			//check each $domain_locale_map key for the tld..
-			//if(preg_match('/^(.*)\b'.$tld.'\b(:[0-9]+)?/',$host)){
-			if(preg_match('!^(.*)\b'.$tld.'\b(/.*$)?!',$host)){
+            //if(preg_match('!^(.*)\b'.$tld.'\b(/.*$)?!',$host)){
+			if($tld==$host){
 				//if its a match, stop and return the tld
 				return $tld;
 		    }
+
 		}
 		//user_error("No Top Level Domain found", E_USER_WARNING);
 		return null;
 	}
-	
+
 	/**
 	 * convertLocaleToTLD
 	 * finds the current locale and returns a domain name (based on current domain name)
@@ -104,20 +105,20 @@ class TranslatableDomains{
 	 *			Translatable class says page is german locale
 	 *			Look up german locale in domain_locale_map
 	 *			return www.mysite.de (with or without end /)
-	 * 
+	 *
 	 * @param boolean $withEndSlash option to receive tld with or without the end slash
 	 * @return string The Top Level Domain that is associated with the locale.
 	 */
-	 
-	 
-	public function convertLocaleToTLD($withEndSlash=true){
+
+
+	public static function convertLocaleToTLD($withEndSlash=true){
 		$tld = self::setDomainByPageLocale($_SERVER["HTTP_HOST"]);
 		return ($withEndSlash) ? $tld.'/' : $tld;
 	}
-	
-	
-	
-	public function setDomainByPageLocale($host){
+
+
+
+	public static function setDomainByPageLocale($host){
 		if($tld = self::getTLD($host)){
 			$currentLocale = Translatable::get_current_locale();
 			$domain = Director::protocolAndHost();
